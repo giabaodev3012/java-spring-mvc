195e325d1605de731c65f9a3c0bf4c1aa63a355d
Nguyen Luong Gia Bao <nguyenluonggiabao301203@gmail.com>
2025-03-06 21:51:02 +0700

    Add DevTools Dependency

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -6,7 +6,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("/")
-    public String index(){
-        return "Hello World with Gia Bao";
+    public String index() {
+        return "Hello World with Gia Bao update";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page";
     }
 }
